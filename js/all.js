c534aeec9ceef6368ab9145477cd5cac231a4db9
maxhyun <40560988+maxhyun@users.noreply.github.com>
2019-04-14 15:44:34 +0800

    22

---
+++ js/all.js
@@ -0,0 +1,88 @@
+var bmi = document.querySelector('.bmiInput');
+var cal = document.querySelector('.bmiCal');
+var cmInput = document.getElementById('cmInput');
+var kgInput = document.getElementById('kgInput');
+var showBmi = document.querySelector('.showBmi');
+var clearData = document.querySelector('.clearData');
+var showData = JSON.parse(localStorage.getItem('bmiData')) || [];
+
+recodeBmi(showData);
+
+cal.addEventListener('click',bmiCal);
+clearData.addEventListener('click',clear);
+
+
+function bmiCal(e) {
+    var cm = (cmInput.value);
+    var kg = kgInput.value;
+    var str = '';
+    //判斷是否為正確數值
+    if(isNaN(cm) || isNaN(kg)){
+        alert('請輸入正確數字');
+        return;
+    }else {
+        str = (kg/(cm*cm/10000)).toFixed(2);
+    }
+    var bmiClass = '';
+    var bmiRg = '';
+    if(str < 18.5){
+        bmiClass = '過輕';
+        bmiRg = '<button type="button" class="btn btn-circle-feather " data-toggle="bmiRg" >'+str+'<p style="font-size:8px; margin-bottom:-5px;">BMI</p></button><div class="text-feather h5 text-center pt-2">'+bmiClass+'</div><a href="#" id="resetimg" style="background-color: #31BAF9;"></a>'
+        cal.innerHTML = bmiRg;
+    }else if (str >=18.5 && str <= 25 ) {
+        bmiClass = '理想';
+        bmiRg = '<button type="button" class="btn btn-circle-good " data-toggle="bmiRg" >'+str+'<p style="font-size:8px; margin-bottom:-5px;">BMI</p></button><div class="text-good h5 text-center pt-2">'+bmiClass+'</div><a href="#" id="resetimg" style="background-color: #86D73F;"></a>'
+        cal.innerHTML = bmiRg;
+    }else if (str >25 && str <= 30 ) {
+        bmiClass = '過重';
+        bmiRg = '<button type="button" class="btn btn-circle-heavy " data-toggle="bmiRg" >'+str+'<p style="font-size:8px; margin-bottom:-5px;">BMI</p></button><div class="text-heavy h5 text-center pt-2">'+bmiClass+'</div><a href="#" id="resetimg" style="background-color:#FF982D;"></a>'
+        cal.innerHTML = bmiRg;
+    }else if (str >30 && str <= 35 ) {
+        bmiClass = '輕度肥胖';
+        bmiRg = '<button type="button" class="btn btn-circle-fat-m " data-toggle="bmiRg" >'+str+'<p style="font-size:8px; margin-bottom:-5px;">BMI</p></button><div class="text-fat-m h5 text-center pt-2">'+bmiClass+'</div><a href="#" id="resetimg" style="background-color: #FF6C03;"></a>'
+        cal.innerHTML = bmiRg;
+    }
+    else if (str >35 ) {
+        bmiClass = '重度肥胖';
+        bmiRg = '<button type="button" class="btn btn-circle-fat-l " data-toggle="bmiRg" >'+str+'<p style="font-size:8px; margin-bottom:-5px;">BMI</p></button><div class="text-fat-l h5 text-center pt-2">'+bmiClass+'</div><a href="#" id="resetimg" style="background-color: #FF1200;"></a>'
+        cal.innerHTML = bmiRg;
+    }
+    //重置
+    document.getElementById('resetimg').onclick = function(e) {
+        e.preventDefault();
+        if(e.target.nodeName == 'A'){
+            e.stopPropagation();
+            window.location.reload();
+        }
+    }
+    var save = {
+        you : cm ,
+        youKg : kg ,
+        youBmi : str,
+        yourClass : bmiClass
+    };
+    showData.push(save);
+    localStorage.setItem('bmiData', JSON.stringify(showData));
+    recodeBmi(showData);
+}
+
+
+function recodeBmi(items) { //items = showdata
+    str = '';
+    var today = new Date();
+    var len = items.length;
+    for (var i = 0; len > i; i++) {
+        str += '<li class="d-flex justify-content-around align-items-center py-3 bg-white mb-3"><span>' + items[i].yourClass + '</span><span><span class="items mr-2">BMI</span>' + items[i].youBmi + '</span><span><span class="items mr-2">weight</span>' + items[i].youKg + '</span><span><span class="items mr-2">height</span>' + items[i].you + '</span><span class="items">'+today.getFullYear()+'/'+today.getMonth()+'/'+today.getDate()+'</span></li>';
+    }
+    showBmi.innerHTML = str;
+}
+
+//清除記錄
+function clear(e){
+    var len = showData.length;
+    showData.splice(0,len);
+    localStorage.setItem('bmiData', JSON.stringify(showData));
+    recodeBmi(showData);
+}
+
+
